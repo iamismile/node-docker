ffd11ad2f319c83e90cc9f53e9a8b70813e1a684
Ismile Hossain <iamismile.dev@gmail.com>
2022-06-13 00:32:48 +0600

    Ensure DB connection

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
-mongoose
-  .connect(mongoURL)
-  .then(() => {
-    console.log('Successfully connected to DB');
-  })
-  .catch((e) => {
-    console.error(e);
-  });
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL)
+    .then(() => {
+      console.log('Successfully connected to DB');
+    })
+    .catch((e) => {
+      console.error(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get('/', (req, res) => {
   res.send('<h2>Hi There</h2>');
